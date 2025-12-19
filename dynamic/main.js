aa23c68dafd2415abd8b1805ad21392fbc2fc70b
Hayato Toyoshima <hayato.toyoshima@g.dreamcareer.co.jp>
2025-12-19 12:12:04 +0900

    commit

---
+++ dynamic/main.js
@@ -0,0 +1,29 @@
+// script.js
+const type = document.getElementById('type');
+const jobField = document.getElementById('jobField');
+const message = document.getElementById('message');
+const count = document.getElementById('count');
+const form = document.getElementById('contactForm');
+const modal = document.getElementById('modal');
+
+if (type) {
+  type.addEventListener('change', () => {
+    jobField.classList.toggle('hidden', type.value !== 'job');
+  });
+}
+
+if (message) {
+  message.addEventListener('input', () => {
+    count.textContent = `${message.value.length} / 100`;
+  });
+}
+
+if (form) {
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    setTimeout(() => {
+      modal.classList.add('hidden');
+    }, 3000);
+  });
+}
