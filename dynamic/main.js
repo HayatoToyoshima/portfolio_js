01f894fbc5c2d417f867aa2acf02f320d4d4fed2
Hayato Toyoshima <hayato.toyoshima@g.dreamcareer.co.jp>
2025-12-19 17:25:59 +0900

    commit

--- dynamic/main.js
+++ dynamic/main.js
@@ -9,6 +9,8 @@ const content = document.getElementById('accordionContent');
 const typeValue = document.getElementById("type").value;
 const jobFieldValue = document.getElementById("jobField").value;
 const messageValue = document.getElementById("message").value.trim();
+console.log(typeValue,jobFieldValue,messageValue);
+
 let hasError = false;
 
 if (type) {
@@ -27,17 +29,17 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
       });
     }
 
-    if (typeValue != '') {
+    if (typeValue) {
       document.getElementById("typeError").textContent = "お問い合わせ種別を選択してください。";
       hasError = true;
     }
 
-    if (typeValue =='job' && jobFieldValue != '') {
+    if (jobFieldValue) {
       document.getElementById("jobFieldError").textContent = "希望職種を選択してください。";
       hasError = true;
     }
 
-    if (messageValue != '') {
+    if (messageValue) {
       document.getElementById("messageError").textContent = "メッセージを入力してください。";
       hasError = true;
     }
